0e9a51ca87fb215b688597d8c47db1b755082c80
monyo <96361228+ynishisaki@users.noreply.github.com>
2024-02-10 18:44:51 +0900

    style: add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 import Script from 'next/script';
 
 class MyDocument extends Document {
@@ -14,7 +14,10 @@ class MyDocument extends Document {
           <meta name='twitter:card' content='summary_large_image' />
           <meta name='twitter:site' content='@monyo75559702' />
           <meta name='twitter:player' content='@monyo75559702' />
-          <link rel='icon' href='/favicon.ico' />
+          <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+          <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+          <link rel='manifest' href='/site.webmanifest' />
           <link
             href='https://fonts.googleapis.com/css2?family=Kosugi+Maru&display=swap'
             rel='stylesheet'
