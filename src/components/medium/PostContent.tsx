94ccff807ea6a2f4d12da1c08fc013642c6e70df
monyo <96361228+ynishisaki@users.noreply.github.com>
2024-05-09 12:37:57 +0000

    style: set the font size for the article

--- src/components/medium/PostContent.tsx
+++ src/components/medium/PostContent.tsx
@@ -1,19 +1,29 @@
-import React, { useEffect } from 'react';
-import 'zenn-content-css';
-import markdownHtml from 'zenn-markdown-html';
-import { getStaticProps } from '../../pages';
+import { Box } from "@chakra-ui/react";
+import { useEffect } from "react";
+import "zenn-content-css";
 
 export const PostContent = ({ content }: { content: string }) => {
-  useEffect(() => {
-    import('zenn-embed-elements');
-  }, []);
+	useEffect(() => {
+		import("zenn-embed-elements");
+	}, []);
 
-  return (
-    <div
-      className='znc'
-      dangerouslySetInnerHTML={{
-        __html: content,
-      }}
-    />
-  );
+	return (
+		<Box
+			className='znc'
+			dangerouslySetInnerHTML={{
+				__html: content,
+			}}
+			sx={{
+				"& h1": {
+					fontWeight: "bold",
+				},
+				"& h2": {
+					fontWeight: "bold",
+				},
+				"& h3": {
+					fontWeight: "semibold",
+				},
+			}}
+		/>
+	);
 };
