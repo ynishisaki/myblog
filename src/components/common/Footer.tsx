31183f38f341ba1759802ccf41d548edd076376e
monyo <96361228+ynishisaki@users.noreply.github.com>
2024-09-26 21:10:11 +0900

    style: post page footer

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -5,48 +5,48 @@ import { FiExternalLink } from "react-icons/fi";
 
 export default function Footer() {
   return (
-    <footer className="relative z-10 h-60 bg-[#efece7] p-4">
-      <div className="flex max-w-xl flex-col justify-center md:mx-auto">
-        <div className="flex items-end">
-          <FcBusinesswoman className="text-3xl" />
-          <span className="text-md font-bold text-slate-900">もにょ</span>
-        </div>
-        <div className="mt-3 text-sm text-slate-900">
-          海なし岐阜県生まれ。今は東京に住んでいる。<br></br>
-          なぜあだ名がもにょなのかというと、「もにょっとしているから」とのこと。
-          <br></br>
-          主な使用言語はTypeScriptとPython。
+    <footer className="relative bg-[#efece7] p-8 shadow-xl shadow-black/10">
+      <div className="mx-auto flex w-[90%] flex-col justify-center gap-4 md:w-[750px]">
+        <div>
+          <div className="flex items-end">
+            <FcBusinesswoman className="text-3xl" />
+            <span className="text-md font-bold text-slate-900">もにょ</span>
+          </div>
+          <div className="mt-2 text-sm text-slate-900">
+            海なし岐阜県生まれ。今は東京に住んでいる。<br></br>
+            なぜあだ名がもにょなのかというと、「もにょっとしているから」とのこと。
+            <br></br>
+            主な使用言語はTypeScriptとPython。
+          </div>
         </div>
 
-        <div className="my-3 border-t border-slate-300" />
+        <div className="border-t border-slate-300" />
 
-        <ul>
-          <li>
+        <div className="grid grid-cols-1 gap-1 sm:grid-cols-2 sm:gap-4">
+          <div>
             <Link
               href="/posts/20220215-privacy-policy/"
               className="text-sm text-slate-900 hover:underline"
             >
               プライバシーポリシー/免責事項
             </Link>
-          </li>
+          </div>
 
-          <li>
+          <div>
             <a
               href="https://github.com/ynishisaki/myblog.git"
               target="_blank"
               rel="noopener noreferrer"
-              className="flex items-center gap-x-1 text-sm text-slate-900 hover:underline"
+              className="inline-flex items-center gap-x-1 text-sm text-slate-900 hover:underline"
             >
               <AiFillGithub />
               お問い合わせはGitHubにて
               <FiExternalLink />
             </a>
-          </li>
-        </ul>
-
-        <div className="mx-auto mt-4 text-sm text-slate-700">
-          © 2022 もにょ
+          </div>
         </div>
+
+        <div className="text-sm text-slate-500">© 2022 もにょ</div>
       </div>
     </footer>
   );
