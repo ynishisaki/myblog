f365cfa3927a44274cbd1bf52b558817fbba6ff1
Your Name <you@example.com>
2024-10-27 15:53:46 +0900

    add: customized nextra-theme-blog

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 import nextra from "nextra";
 
 const withNextra = nextra({
-  // theme: "./theme.tsx",
   theme: "nextra-theme-blog",
-  // theme: "nextra-theme-docs",
   themeConfig: "./theme.config.jsx",
   defaultShowCopyCode: true,
   latex: true,
